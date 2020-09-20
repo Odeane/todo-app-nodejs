83a5bb440fd16e1d4ef9f2fa1254e14bb9fa6591
odeane_croney <odeane_croney@yahoo.com>
2020-09-20 21:48:44 +0200

    displaying data from db

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let db;
 
 
 //setting up connection to the mongo db database 
-let connectionString = 'mongodb+srv://odeane:<password>@cluster0.iaw7w.mongodb.net/TodoApp?retryWrites=true&w=majority' //add password for it to work
+let connectionString = 'mongodb+srv://odeane:mother30@cluster0.iaw7w.mongodb.net/TodoApp?retryWrites=true&w=majority' //add password for it to work
 mongodb.connect(connectionString, { useNewUrlParser: true }, function (err, client) {
   app.listen(3000)
   db = client.db()
@@ -15,7 +15,8 @@ mongodb.connect(connectionString, { useNewUrlParser: true }, function (err, clie
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
-  res.send(`<!DOCTYPE html>
+  db.collection('items').find().toArray(function (err, items) {
+    res.send(`<!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8">
@@ -37,38 +38,27 @@ app.get('/', function (req, res) {
     </div>
     
     <ul class="list-group pb-5">
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #1</span>
+      ${items.map(function (item) {
+        return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+        <span class="item-text">${item.text}</span>
         <div>
           <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
           <button class="delete-me btn btn-danger btn-sm">Delete</button>
         </div>
-      </li>
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #2</span>
-        <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-      </li>
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #3</span>
-        <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-      </li>
+      </li>`
+      }).join('')}
     </ul>
     
   </div>
   
 </body>
 </html>`)
+  }) 
 })
 
 app.post('/create-item', function (req, res) {
   db.collection('items').insertOne({ text: req.body.item }, function () {
-    res.send("thanks for submitting")
+    res.redirect('/')
   })
 
 })
